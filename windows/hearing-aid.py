52b4d07c0eba1f4c13fec77fa022a8bb7c5f9125
Kavish Devar <mail@kavishdevar.me>
2025-11-28 14:04:03 +0530

    windows: remove unnecessary sys path modification

--- windows/hearing-aid.py
+++ windows/hearing-aid.py
@@ -23,9 +23,6 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal, QObject
 
-import os
-sys.path.insert(0, os.path.dirname(__file__))
-
 handler = logging.StreamHandler()
 class ColorFormatter(logging.Formatter):
     COLORS = {
